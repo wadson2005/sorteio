f623589b59d84c653d3144debd628d725a1fd71d
wadson2005 <wadsondias62@gmail.com>
2023-12-18 15:36:24 -0300

    versão quase pronta

--- sorteio.py
+++ sorteio.py
@@ -1,19 +1,98 @@
 import random
-participantes = {}
-while True:
-    menu = input(f"digite 'adicionar' para adicionar um participante, \n tecle 'exit' para enserrar o programa").lower()
-    if menu == 'adicionar':
-        nome = input("Digite o nome do participantes a ser adicionado ao sorteio:").lower().strip()
-        numero = int(input(f"digite o número escolhido por {nome}"))
-        participantes[nome] = [numero]
-    elif menu == 'mostrar':
-        for name, nun in participantes.items():
-            print(f'{name} {nun}')
-    elif menu == 'exit':
-        break
-    elif menu == 'sortear':
-        vencedor_nome = random.choice(list(participantes.keys()))
-        vencedor_numero = participantes[vencedor_nome]
-        print(f'O vencedor é: \n {vencedor_nome} com o número {vencedor_numero}!')
+import json
+import os
+
+DIRETORIO = 'files'
+
+def carregar_participantes():
+    try:
+        with open(os.path.join(DIRETORIO, 'participantes.txt'), 'r') as arquivo:
+            participantes = json.load(arquivo)
+    except (FileNotFoundError, json.JSONDecodeError):
+        participantes = {}
+    return participantes
+
+def salvar_participantes(participantes):
+    if not os.path.exists(DIRETORIO):
+        os.makedirs(DIRETORIO)
+
+    with open(os.path.join(DIRETORIO, 'participantes.txt'), 'w') as arquivo:
+        json.dump(participantes, arquivo)
+
+def adicionar_participante(participantes):
+    numero = int(input("Digite o número escolhido (entre 0 e 120): "))
+    
+    if 0 <= numero <= 120:
+        if not any(numero in nums['numeros'] for nums in participantes.values()):
+            nome = input("Digite o nome do participante: ").lower().strip()
+            telefone = input("Digite o número de telefone do participante: ")
+            
+            if nome not in participantes:
+                participantes[nome] = {'numeros': [numero], 'telefone': telefone}
+            else:
+                participantes[nome]['numeros'].append(numero)
+                participantes[nome]['telefone'] = telefone
+
+            salvar_participantes(participantes)
+            print(f"numero {numero} adicionado com sucesso para o participante {nome}!")
+        else:
+            print(f"O número {numero} já está sendo utilizado por outro participante.")
     else:
-        print('opção inválida.')
+        print("Número fora do intervalo permitido. Digite um número entre 0 e 120.")
+
+def excluir_participante(participantes):
+    nome = input("Digite o nome do participante que deseja excluir: ").lower().strip()
+    if nome in participantes:
+        numero_para_excluir = int(input("Digite o número que deseja excluir: "))
+        if numero_para_excluir in participantes[nome]['numeros']:
+            participantes[nome]['numeros'].remove(numero_para_excluir)
+            if not participantes[nome]['numeros']:
+                del participantes[nome]
+            salvar_participantes(participantes)
+            print(f"Número {numero_para_excluir} excluído com sucesso para o participante {nome}!")
+        else:
+            print(f"O número {numero_para_excluir} não pertence ao participante {nome}.")
+    else:
+        print(f"O participante {nome} não foi encontrado.")
+
+def mostrar_participantes(participantes):
+    for nome, dados in participantes.items():
+        numeros = ', '.join(map(str, dados['numeros']))
+        telefone = dados['telefone']
+        print(f'{nome}: Números: {numeros}, Telefone: {telefone}')
+    if not participantes:
+        print('Não há participantes cadastrados.')
+
+def main():
+    participantes = carregar_participantes()
+
+    while True:
+        menu = input(f"Digite 'adicionar' para acrescentar um número ao sorteio, \n'excluir' para remover um número que já está na lista de sorteio, \n'mostrar' para exibir as informações já cadastradas,"
+                     f"\n'sortear' para realizar o sorteio, ou 'exit' para encerrar o programa: ").lower()
+
+        if menu == 'adicionar':
+            adicionar_participante(participantes)
+
+        elif menu == "excluir":
+            excluir_participante(participantes)
+        elif menu == 'mostrar':
+            mostrar_participantes(participantes)
+
+        elif menu == 'sortear':
+            if participantes:
+                vencedor_nome = random.choice(list(participantes.keys()))
+                vencedor_dados = participantes[vencedor_nome]
+                vencedor_numero = random.choice(vencedor_dados['numeros'])
+                vencedor_telefone = vencedor_dados['telefone']
+                print(f"O vencedor é:\nNome: {vencedor_nome}, Número: {vencedor_numero}, Telefone: {vencedor_telefone}!")
+            else:
+                print("Não há participantes para realizar o sorteio.")
+
+        elif menu == 'exit':
+            break
+
+        else:
+            print('Opção inválida.')
+
+if __name__ == "__main__":
+    main()
